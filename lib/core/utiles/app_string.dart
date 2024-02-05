7cb06502002829d38cdf47cbe463c7b32876135b
Hesham Gasser <heshamgasser32@gmail.com>
2024-02-05 18:54:34 +0300

    complete home screen UI

---
+++ lib/core/utiles/app_string.dart
@@ -0,0 +1,8 @@
+class AppStrings {
+
+  static const String bestSeller = 'Best Seller';
+  static const String bookName = 'Harry Potter and the Goblet of Fire';
+
+
+
+}
--- lib/features/home_screen/presentation/pages/home_screen.dart
+++ lib/features/home_screen/presentation/pages/home_screen.dart
@@ -1,6 +1,10 @@
 import 'package:bookly/core/utiles/app_images.dart';
-import 'package:bookly/features/home_screen/presentation/pages/books_slider_item.dart';
+import 'package:bookly/core/utiles/app_string.dart';
+import 'package:bookly/core/utiles/app_text_styles.dart';
+import 'package:bookly/features/home_screen/presentation/widgets/best_seller_items.dart';
+import 'package:bookly/features/home_screen/presentation/widgets/books_slider_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,12 +15,29 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Image.asset(AppImages.appLogo),
         actions: [
-          IconButton(onPressed: () {
-
-          }, icon: Icon(Icons.search),),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
         ],
       ),
-      body: BooksSliderItems(),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            BooksSliderItems(),
+            SizedBox(height: 50.h),
+            Text(
+              AppStrings.bestSeller,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            SizedBox(height: 19.h),
+            BestSellerItems(),
+
+          ],
+        ),
+      ),
     );
   }
 }
