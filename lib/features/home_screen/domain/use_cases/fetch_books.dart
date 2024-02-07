7ba823c3e1f1dd7e5d832aa4c14de05933155d61
Hesham Gasser <heshamgasser32@gmail.com>
2024-02-07 23:42:28 +0300

    complete domain layer

--- lib/features/home_screen/domain/use_cases/fetch_books.dart
+++ lib/features/home_screen/domain/use_cases/fetch_books.dart
@@ -1,15 +1,23 @@
 import 'package:bookly/core/errors/failure.dart';
 import 'package:bookly/features/home_screen/domain/entities/book_entity.dart';
 import 'package:bookly/features/home_screen/domain/repositories/home_repo.dart';
+import 'package:bookly/features/home_screen/domain/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchBooksUseCases {
+class FetchBooksUseCases extends UseCase<List<BookEntity>, NoParam> {
   HomeRepo homeRepo;
+
 
   FetchBooksUseCases(this.homeRepo);
 
-  Future<Either<Failure, List<BookEntity>>> call (){
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) {
     return homeRepo.fetchBooks();
   }
+
+
+
+
+
 
 }
